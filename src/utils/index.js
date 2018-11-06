1c958a3c09fd06ecfc2ae20f07a1c32a5792c7be
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2018-11-06 11:42:49 +0300

    MEDIA-1796 Fixed styles for separator, fixed case when last seen message not presented

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,7 +14,7 @@ export const Debug = (namespace) => {
 }
 
 export const getIndexById = (id, array) => {
-  let index
+  let index = null
 
   for (let i = 0; i < array.length; i++) {
     if (array[i].id === id) {
