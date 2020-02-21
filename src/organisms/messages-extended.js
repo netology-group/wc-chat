76eb55add5a7aa8a9693a897283d465484d5f082
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2020-02-21 17:33:26 +0300

    ULMS-288 Fixed error with actions

--- src/organisms/messages-extended.js
+++ src/organisms/messages-extended.js
@@ -225,10 +225,10 @@ export class _XMessagesElement extends MessagesElement {
     Array.isArray(this.actions) &&
       this.actions.forEach(_ => {
         const key = _[0];
-        const _action = this._actions.get(key) || {};
+        const _action = this._actions && this._actions.get(key) || {};
 
         if (!isAllowed(_action, data)) {
-          debug('Action is not alllowed');
+          debug('Action is not allowed');
 
           return;
         }
