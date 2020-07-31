e2ee5649eefce86bc77e89b792c622af6dc37bbb
Sergo <rogaldh@radsh.red>
2020-07-31 14:42:06 +0300

    Allow to group messages from same author per interval

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,6 +18,13 @@ export const debug = namespace => {
 
 const deb = debug('@ulms/wc-chat/utils');
 
+const getUTCDate = d => `${d.getUTCFullYear()}${d.getUTCMonth()}${d.getUTCDate()}`;
+
+const getUTCDateTimeAsS = d =>
+  `${d.getUTCFullYear()}${d.getUTCMonth()}${d.getUTCDate()}${d.getUTCHours() * 3600 +
+    d.getUTCMinutes() * 60 +
+    d.getUTCSeconds()}`;
+
 export const stampToDate = stamp => new Date(stamp * 1e3);
 
 export const formatDate = (date, pattern = /\d{2}:\d{2}/) => date.toTimeString().match(pattern);
@@ -41,6 +48,17 @@ export const readableTime = maybeDate => {
 
 export const isAggregatedBy = (field, index, list) =>
   !index || !field ? false : list[index][field] === list[index - 1][field];
+
+export const isAggregatedByDate = (field, index, list, interval) => {
+  const prev = list[index - 1][field];
+  const next = list[index][field];
+
+  return !index || !field
+    ? false
+    : typeof interval === 'undefined'
+    ? getUTCDate(new Date(next)) === getUTCDate(new Date(prev))
+    : getUTCDateTimeAsS(new Date(next)) - getUTCDateTimeAsS(new Date(prev)) <= interval;
+};
 
 export const requestAnimation = fn => {
   const { requestAnimationFrame } = globalThis;
