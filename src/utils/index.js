7ae1af8c6342b499fdc8709eb0107c824660baba
Sergo <rogaldh@radsh.red>
2020-08-04 12:46:53 +0300

    Fix timestamps

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,13 +18,6 @@ export const debug = namespace => {
 
 const deb = debug('@ulms/wc-chat/utils');
 
-const getISODate = d => d.toISOString().match(/\d{4}-\d{2}-\d{2}/)[0];
-
-const getNumericYMD = d => Number(getISODate(d).replaceAll('-', ''));
-
-const getNumericYMDS = d =>
-  `${getNumericYMD(d)}${d.getUTCHours() * 3600 + d.getUTCMinutes() * 60 + d.getUTCSeconds()}`;
-
 export const stampToDate = stamp => new Date(stamp * 1e3);
 
 export const formatDate = (date, pattern = /\d{2}:\d{2}/) => date.toTimeString().match(pattern);
@@ -49,15 +42,22 @@ export const readableTime = maybeDate => {
 export const isAggregatedBy = (field, index, list) =>
   !index || !field ? false : list[index][field] === list[index - 1][field];
 
-export const isAggregatedByDate = (field, index, list, interval) => {
+export const isAggregatedByDate = (field, index, list, interval = 86400) => {
+  if (!index || !field) return false;
+
   const prev = list[index - 1][field];
   const next = list[index][field];
 
-  return !index || !field
-    ? false
-    : typeof interval === 'undefined'
-    ? getNumericYMD(new Date(next)) === getNumericYMD(new Date(prev))
-    : getNumericYMDS(new Date(next)) - getNumericYMDS(new Date(prev)) <= interval;
+  if (!prev || !next) return false;
+
+  const nextD = new Date(next);
+  const prevD = new Date(prev);
+
+  if (isNaN(nextD.getTime()) || isNaN(prevD.getTime())) return false;
+
+  return (
+    nextD.getTime() > prevD.getTime() && nextD.getTime() / 1e3 - prevD.getTime() / 1e3 <= interval
+  );
 };
 
 export const requestAnimation = fn => {
